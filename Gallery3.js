e71afc410b7af42f4e6fc5369fb4765f40051c29
xygzhou <47159663+xygzhou@users.noreply.github.com>
2020-07-26 22:51:53 -0400

    initial commit

---
+++ Gallery3.js
@@ -0,0 +1,53 @@
+// This is my first javascript code practice
+
+// console.log("Hello World");
+// let name = 'Gabby'; //String literal
+// console.log(name);
+
+// data types //
+// let lastName = null;
+// let firstName = undefined; 
+
+// create a conflict, const should be undefineable
+// const interestRate = 0.3;
+// interestRate = 1; //Number literal
+// console.log(interestRate);
+
+// let isApproved = true; //Boolean literal
+// let name = 'Gabby';
+// let age = 23;
+
+// let person = {
+// 	name: 'Gabby', 
+// 	age: 23
+// };
+
+// change attributes //
+// person.name = 'Elle'; //change attribute
+
+// person['name'] = 'Mary';
+
+// let selection = 'name';
+// person[selection] = 'Grace';
+
+// console.log(person);
+
+// Arrays //
+// let selectedColors = ['red','blue'];
+// selectedColors[2] = 21;
+// console.log(selectedColors.length);
+
+// Functions //
+// function greet(firstName,lastName) {
+// 	console.log('Hello, ' + firstName + ' ' + lastName + '!');
+// }
+
+// greet('Gabby','Zhou');
+// greet('Jin');
+
+
+// function square (number) {
+// 	return number * number;
+// }
+
+// console.log(square(2));
